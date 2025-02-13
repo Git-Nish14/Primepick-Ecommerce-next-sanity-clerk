1c69b935e0e96728a7b1768502835b812f0556bd
Nish <nish14062003@gmail.com>
2025-02-13 12:44:59 +0530

    first commit

---
+++ src/app/(store)/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+function page() {
+  return (
+    <div>
+      <h1>hello world 123</h1>
+      <Button>Click me </Button>
+    </div>
+  );
+}
+
+export default page;
