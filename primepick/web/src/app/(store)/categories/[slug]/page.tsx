c8ba6817444d1546bfb1e3f0806e70594b33992c
Nish <nish14062003@gmail.com>
2025-02-19 12:12:53 +0530

    loader added  and category page

--- primepick/web/src/app/(store)/categories/[slug]/page.tsx
+++ primepick/web/src/app/(store)/categories/[slug]/page.tsx
@@ -1,5 +1,27 @@
-function Categorypage() {
-  return <div>category</div>;
+import ProductsView from "@/components/productsView";
+import { getAllCategories } from "../../../../../lib/products/getAllCategories";
+import { getProductsByCategory } from "../../../../../lib/products/getProductsByCategory";
+
+async function CategoryPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
+
+  const products = await getProductsByCategory(slug);
+  const categories = await getAllCategories();
+
+  return (
+    <div className="flex felx col items-center justify-top min-h-screen bg-gray-100 p-4">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
+        <h1 className="text-3xl font-bold mb-6 text-center">
+          {slug
+            .split("-")
+            .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+            .join("")}{" "}
+          collection
+        </h1>
+        <ProductsView products={products} categories={categories} />
+      </div>
+    </div>
+  );
 }
 
-export default Categorypage;
+export default CategoryPage;
---
+++ primepick/web/src/components/Loader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loader;
