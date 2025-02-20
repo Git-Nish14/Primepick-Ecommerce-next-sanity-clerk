5fc4cc41cb135233cd8f701ce10f1a78cc94de64
Nish <nish14062003@gmail.com>
2025-02-20 15:49:12 +0530

    error resolved

--- primepick/sanity/sanity.cli.ts
+++ primepick/sanity/sanity.cli.ts
@@ -3,8 +3,10 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'l8wch8i3',
-    dataset: 'primepick'
+    dataset: 'primepick',
   },
+
+  studioHost: 'ecommercebuild',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
